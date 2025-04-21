30b045182a0a53b260dca72bc4e3c0bbe39f4253
Bantu-art <brianbaraza40@gmail.com>
2025-04-21 14:58:06 +0300

    Implement custom user manager with user and superuser creation methods

--- auth_service/accounts/models.py
+++ auth_service/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, phone, password=None):
+        if not email or not phone:
+            raise ValueError("Users must have an email and phone number")
+        user = self.model(email=self.normalize_email(email), phone=phone)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, phone, password):
+        user = self.create_user(email, phone, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
